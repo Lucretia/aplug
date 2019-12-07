00f2e86afd12a2134e09dedade3cf6f19a19dc08
Luke A. Guest <laguest@archeia.com>
2019-12-07 15:57:22 +0000

    Basic framework which is detected by a LADSPA host.

--- ladspa/examples/amp.adb
+++ ladspa/examples/amp.adb
@@ -1,6 +1,11 @@
 with System;
 
 package body Amp is
+   type Amplifiers is
+      record
+         Control_Value : access LADSPA.Data;
+      end record;
+
    function Instantiate (Descriptor  : access constant LADSPA.Descriptors;
                          Sample_Rate : C.unsigned_long) return LADSPA.Handles
    is
@@ -8,4 +13,66 @@ package body Amp is
       return LADSPA.Handles (System.Null_Address);
    end Instantiate;
 
+   procedure Connect_Port (Instance      : in LADSPA.Handles;
+                           Port          : in C.unsigned_long;
+                           Data_Location : access LADSPA.Data) is
+
+      Actual_Port : constant Mono_Port_Numbers := Mono_Port_Numbers'Val (Port);
+   begin
+      case Actual_Port is
+         when Control =>
+            null;
+         when Input_1 =>
+            null;
+         when Output_1 =>
+            null;
+      end case;
+   end Connect_Port;
+
+   procedure Activate (Instance : in out LADSPA.Handles) is
+   begin
+      null;
+   end Activate;
+
+   -- procedure Deactivate (Instance : in out Handles) is
+   -- begin
+   --    null;
+   -- end;
+
+   procedure Run (Instance : in out LADSPA.Handles; Sample_Count : in C.unsigned_long) is
+   begin
+      null;
+   end Run;
+
+   -- procedure Run_Adding (Instance : in out Handles; Sample_Count : in unsigned_long) is
+   -- begin
+   --    null;
+   -- end;
+
+   -- procedure Run_Adding_Gain (Instance : in out Handles; Gain : in Data) is
+   -- begin
+   --    null;
+   -- end;
+
+   procedure Clean_Up (Instance : in out LADSPA.Handles) is
+   begin
+      for C_Str in Mono_Port_Numbers loop
+         C.Strings.Free (Mono_Port_Names (C_Str));
+      end loop;
+
+      C.Strings.Free (Mono_Descriptor.Label);
+      C.Strings.Free (Mono_Descriptor.Name);
+      C.Strings.Free (Mono_Descriptor.Maker);
+      C.Strings.Free (Mono_Descriptor.Copyright);
+   end Clean_Up;
+
+   function Descriptor (Index : C.unsigned_long) return access constant LADSPA.Descriptors is
+   begin
+      case Index is
+         when 0 =>
+            return Mono_Descriptor'Access;
+         when others =>
+            raise Constraint_Error;
+      end case;
+   end Descriptor;
 end Amp;
--- ladspa/examples/amp.ads
+++ ladspa/examples/amp.ads
@@ -3,14 +3,96 @@
 --
 ------------------------------------------------------------------------------------------------------------------------
 with Interfaces.C;
+with Interfaces.C.Strings;
 with LADSPA;
 
 package Amp is
-   pragma Preelaborate;
+   -- pragma Preelaborate;
 
    package C renames Interfaces.C;
+
+   type Port_Numbers is (Control, Input_1, Output_1, Input_2, Output_2) with
+     Convention => C;
+   subtype Mono_Port_Numbers is Port_Numbers range Control .. Output_1;
 
    function Instantiate (Descriptor  : access constant LADSPA.Descriptors;
                          Sample_Rate : C.unsigned_long) return LADSPA.Handles with
      Convention => C;
+
+   procedure Connect_Port (Instance      : in LADSPA.Handles;
+                           Port          : in C.unsigned_long;
+                           Data_Location : access LADSPA.Data) with
+     Convention => C;
+
+   -- procedure Activate (Instance : in out Handles) with
+   --   Convention => C;
+
+   -- procedure Deactivate (Instance : in out Handles) with
+   --   Convention => C;
+
+   procedure Run (Instance : in out LADSPA.Handles; Sample_Count : in C.unsigned_long) with
+     Convention => C;
+
+   -- procedure Run_Adding (Instance : in out Handles; Sample_Count : in unsigned_long) with
+   --   Convention => C;
+
+   -- procedure Run_Adding_Gain (Instance : in out Handles; Gain : in Data) with
+   --   Convention => C;
+
+   procedure Clean_Up (Instance : in out LADSPA.Handles) with
+     Convention => C;
+private
+   use type LADSPA.All_Port_Descriptors;
+   use type LADSPA.Port_Range_Hint_Descriptors;
+
+   type Mono_Port_Descriptor_Array is array (Mono_Port_Numbers) of LADSPA.All_Port_Descriptors with
+     Convention => C;
+
+   Mono_Port_Descriptors : aliased constant Mono_Port_Descriptor_Array :=
+     (Control  => (LADSPA.Input or LADSPA.Control),
+      Input_1  => (LADSPA.Input or LADSPA.Audio),
+      Output_1 => (LADSPA.Output or LADSPA.Audio));
+
+   type Mono_Port_Names_Array is array (Mono_Port_Numbers) of C.Strings.chars_ptr with
+     Convention => C;
+
+   Mono_Port_Names : Mono_Port_Names_Array :=
+     (Control  => C.Strings.New_String ("Gain"),
+      Input_1  => C.Strings.New_String ("Input"),
+      Output_1 => C.Strings.New_String ("Output"));
+
+   type Mono_Port_Range_Hints_Array is array (Mono_Port_Numbers) of LADSPA.All_Port_Range_Hints with
+     Convention => C;
+
+   Mono_Port_Range_Hints : constant Mono_Port_Range_Hints_Array :=
+     (Control  => (Hint_Descriptor => LADSPA.Bounded_Below or LADSPA.Logarithmic or LADSPA.Default_1,
+                   Lower_Bound     => 0.0,
+                   Upper_Bound     => <>),
+      Input_1  => (Hint_Descriptor => LADSPA.Default_None,
+                   others          => <>),
+      Output_1 => (Hint_Descriptor => LADSPA.Default_None,
+                   others          => <>));
+
+   Mono_Descriptor : aliased LADSPA.Descriptors :=
+     (Unique_ID        => 1048,
+      Label            => C.Strings.New_String ("amp_mono"),
+      Properties       => LADSPA.Hard_RT_Capable,
+      Name             => C.Strings.New_String ("Mono Amplifier"),
+      Maker            => C.Strings.New_String ("Richard Furse (LADSPA example plugins) & Luke A. Guest (Ada port)"),
+      Copyright        => C.Strings.New_String ("None"),
+      Port_Count       => 3,
+      Port_Descriptors => Mono_Port_Descriptors'Address,
+      Port_Names       => Mono_Port_Names'Address,
+      Port_Range_Hints => Mono_Port_Range_Hints'Address,
+      Instantiate      => Instantiate'Access,
+      Connect_Port     => Connect_Port'Access,
+      -- Activate         => Activate'Access,
+      Run              => Run'Access,
+      Clean_Up         => Clean_Up'Access,
+      others           => <>);
+
+   function Descriptor (Index : C.unsigned_long) return access constant LADSPA.Descriptors with
+     Export        => True,
+     Convention    => C,
+     External_Name => "ladspa_descriptor";
 end Amp;
